4c46043118a498ec24348d51106512e33ec386d6
claudio angrigiani <claudio@onefootprint.com>
2025-02-13 17:27:35 -0500

    biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@ const Home = () => {
 		<div className="flex flex-col max-w-[800px] mx-auto justify-center h-screen">
 			<h1 className="">Hey there, I&apos;m Claudio.</h1>
 			<p className="text-body-1">
-				I&apos;m a Design Engineer on the East Coast, building products with
-				care and attention to detail—making the internet a little better, one
-				pixel at a time.
+				I&apos;m a Design Engineer on the East Coast, building products with care and attention to detail—making the
+				internet a little better, one pixel at a time.
 			</p>
 			<a href="https://www.google.com" className="text-label-1 text-accent">
 				More about me
