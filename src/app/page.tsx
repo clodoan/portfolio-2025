07840a758b9ba45db94a1b83cdc3bc464c709cd5
Claudio Angrigiani <angrigianicr@gmail.com>
2025-07-11 23:30:00 -0400

    fix width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
 
   return (
     <div className="w-screen flex flex-col text-primary min-h-screen">
-      <div className="flex flex-col gap-4 max-w-container-mobile md:max-w-container-desktop mx-auto px-5 py-24 rounded overflow-hidden motion-opacity-in-0 motion-blur-in-md motion-duration-1500 z-0">
+      <div className="flex flex-col gap-4 max-w-container-mobile md:max-w-container-desktop mx-auto py-24 rounded overflow-hidden motion-opacity-in-0 motion-blur-in-md motion-duration-1500 z-0">
         <MDXRemote source={content} components={components} />
       </div>
       <Footer
